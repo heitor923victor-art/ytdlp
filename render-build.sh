3846739c2fa9443f16b4e4004b7c558b83c42a1d
heitor923victor-art <heitor923victor@gmail.com>
2026-06-07 16:56:40 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
 npm install
-pip install yt-dlp
+
+python -m pip install yt-dlp
+
+which python || true
+which pip || true
+which yt-dlp || true
+
+find / -name yt-dlp 2>/dev/null | head -20
